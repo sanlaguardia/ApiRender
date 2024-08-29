626ff356829a1aec98ba9182374a1b1a0e167d60
sanlaguardia <dieferlaguardia@gmail.com>
2024-08-28 19:34:21 -0600

    cuartacarga

--- app/routes/routers.js
+++ app/routes/routers.js
@@ -10,10 +10,10 @@ router.get('/api/departamentos/onebyid/:id', departamentos.getDepartamentoById);
 router.put('/api/departamentos/update/:id', departamentos.updateDepartamentoById);
 router.delete('/api/departamentos/delete/:id', departamentos.deleteDepartamentoById);
 
-router.post('/api/departamentos/create', departamentos.createEmpleado);
-router.get('/api/departamentos/all', empleados.retrieveAllEmpleados);
-router.get('/api/departamentos/onebyid/:id', empleados.getEmpleadoById);
-router.put('/api/departamentos/update/:id', empleados.updateEmpleadoById);
-router.delete('/api/departamentos/delete/:id', empleados.deleteEmpleadoById);
+router.post('/api/empleados/create', departamentos.createEmpleado);
+router.get('/api/empleados/all', departamentos.retrieveAllEmpleados);
+router.get('/api/empleados/onebyid/:id', departamentos.getEmpleadoById);
+router.put('/api/empleados/update/:id', departamentos.updateEmpleadoById);
+router.delete('/api/empleados/delete/:id', departamentos.deleteEmpleadoById);
 
 module.exports = router;
