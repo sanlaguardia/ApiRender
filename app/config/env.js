5311d89ccbf17894668a1dfecc04c36dc188d03b
sanlaguardia <dieferlaguardia@gmail.com>
2024-08-28 18:51:28 -0600

    terceracarga

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'umg_antigua2024_4468',
     username: 'umg_antigua2024_4468_user',
     password: '6kkyP7QeOKr97MuaJE26g4aNQWj0pxeb',
-    host: 'dpg-cr6jnrij1k6c73d5f6bg-a',
+    host: 'dpg-cr6jnrij1k6c73d5f6bg-a.oregon-postgres.render.com',
     dialect: 'postgres',
     pool: {
         max: 5,
