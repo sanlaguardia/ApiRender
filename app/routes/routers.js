80cac8143adf1680bb10c37e425075e082f6341c
sanlaguardia <dieferlaguardia@gmail.com>
2024-08-28 19:29:50 -0600

    controlador

--- app/routes/routers.js
+++ app/routes/routers.js
@@ -4,18 +4,16 @@ const router = express.Router();
  
 const departamentos = require('../controllers/controller.js');
 
-router.post('/api/departamentoss/create', departamentos.createDepartamento);
+router.post('/api/departamentos/create', departamentos.createDepartamento);
 router.get('/api/departamentos/all', departamentos.retrieveAllDepartamentos);
 router.get('/api/departamentos/onebyid/:id', departamentos.getDepartamentoById);
 router.put('/api/departamentos/update/:id', departamentos.updateDepartamentoById);
 router.delete('/api/departamentos/delete/:id', departamentos.deleteDepartamentoById);
 
-const empleados = require('../controllers/controller.js');
-
-router.post('/api/empleados/create', empleados.createEmpleado);
-router.get('/api/empleados/all', empleados.retrieveAllEmpleados);
-router.get('/api/empleados/onebyorderid/:id', empleados.getEmpleadoById);
-router.put('/api/empleados/update/:id', empleados.updateEmpleadoById);
-router.delete('/api/empleados/delete/:id', empleados.deleteEmpleadoById);
+router.post('/api/departamentos/create', departamentos.createEmpleado);
+router.get('/api/departamentos/all', empleados.retrieveAllEmpleados);
+router.get('/api/departamentos/onebyid/:id', empleados.getEmpleadoById);
+router.put('/api/departamentos/update/:id', empleados.updateEmpleadoById);
+router.delete('/api/departamentos/delete/:id', empleados.deleteEmpleadoById);
 
 module.exports = router;
